183e064e8ac5fcd4bbc51c1884b2fa42a4e520b6
Becky Nelson <dekarrin.irl@gmail.com>
2021-09-12 12:36:33 -0500

    Added put topic

--- docstore/urls.py
+++ docstore/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import TopicView
 
 urlpatterns = [
+    path('topics/', TopicView.as_view()),
     path('topics/<uuid:topic_id>', TopicView.as_view()),
 ]
--- docstore/views.py
+++ docstore/views.py
@@ -1,23 +1,47 @@
 import uuid
 
 from django.http import JsonResponse, HttpResponseNotFound
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotAllowed, HttpResponseServerError
 from django.views import View
 from django.core.exceptions import ObjectDoesNotExist
 
+
+from . import util
 from .models import Topic
-from .util import serialize_models
 
 # Create your views here.
 class TopicView(View):
 
-    def get(self, request, topic_id: uuid.UUID):
+    def put(self, request, topic_id: uuid.UUID=None):
+        if topic_id is None:
+            return JsonResponse(status=405, data={'error': 'Method PUT not allowed at this resource'})
+
+        payload = request.read()
+        try:
+            parsed = util.deserialize_models(payload, expect_single_object=True, default_id=str(topic_id))
+        except util.ModelParseError as e:
+            return JsonResponse(status=400, data={'error': "Body does not contain a valid Topic: {!s}".format(e)})
+
+        # deserialize_models will have ensured that there is only one item here:
+        for deserialized_object in parsed:
+            deserialized_object.id = topic_id
+            deserialized_object.save()
+
+        # success
+        return JsonResponse(status=204)
+        
+
+    def get(self, request, topic_id: uuid.UUID=None):
+        if topic_id is None:
+            json_data = util.serialize_models(Topic.objects.all())
+            return HttpResponse(json_data, content_type="application/json")
+
         try:
             t = Topic.objects.get(id=topic_id)
         except ObjectDoesNotExist:
-            return JsonResponse(data={'error': 'The requested topic does not exist'}, status=404)
+            return JsonResponse(status=404, data={'error': 'The requested topic does not exist'})
         
-        json_data = serialize_models(t)
+        json_data = util.serialize_models(t)
         # Not sure how to get JsonResponse to accept Model directly; it seems to
         # want a dict but django's core JSON serialize() func does not
         # produce that.
