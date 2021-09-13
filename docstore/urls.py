0cd564e7cfe9aeae4c89fd6fc81398fa2859c857
Becky Nelson <dekarrin.irl@gmail.com>
2021-09-12 21:44:25 -0500

    Everything ends in slash

--- docstore/urls.py
+++ docstore/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('topics/', TopicListView.as_view()),
-    path('topics/<uuid:topic_id>', TopicDetailView.as_view()),
+    path('topics/<uuid:topic_id>/', TopicDetailView.as_view()),
     path('folders/', FolderListView.as_view()),
-    path('folders/<uuid:folder_id>', FolderDetailView.as_view()),
+    path('folders/<uuid:folder_id>/', FolderDetailView.as_view()),
     path('documents/', DocumentListView.as_view()),
-    path('documents/<uuid:doc_id>', DocumentDetailView.as_view()),
+    path('documents/<uuid:doc_id>/', DocumentDetailView.as_view()),
 ]
