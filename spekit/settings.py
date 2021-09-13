b868f706b624b938815739ee1cc0e964042e47db
Becky Nelson <dekarrin.irl@gmail.com>
2021-09-12 19:07:35 -0500

    basic hiding of secrets in prod

--- spekit/settings.py
+++ spekit/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hnf#24=&u=yr-7hv07mqrxycdr@fzlm27+mgv@5erxh-^^t@s$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-hnf#24=&u=yr-7hv07mqrxycdr@fzlm27+mgv@5erxh-^^t@s$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
